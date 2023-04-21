e0bbd0314c58f8207718b38854cb1bbb7e0ebd8f
obscraft23 <obscraft23@gmail.com>
2023-04-21 18:37:57 +0900

    add class WsSever to app.py

--- app.py
+++ app.py
@@ -9,88 +9,115 @@ async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
+class WsServer:
+    def start(self,host="0.0.0.0",port=19132):
+        self.host = host
+        self.port = port
+        self.ws = websockets.serve(self.receive, self.host, self.port, process_request=self.health_check)
+        self.loop = asyncio.get_event_loop()
+        self.loop.run_until_complete(self.ws)
+        self.loop.run_forever()
 
-async def health_check(path, request_headers):
-    if path == "/healthz":
-        return http.HTTPStatus.OK, [], b"OK\n"
+    async def health_check(self,path, request_headers):
+        if path == "/healthz":
+            return http.HTTPStatus.OK, [], b"OK\n"
 
-async def receive(websocket):
-    ws = websocket
-    await listen_event(ws)
-    await event("connect")
-    try:
-        while True:
-            data = await ws.recv()
-            await parse_command(json.loads(data))
-    except (websockets.exceptions.ConnectionClosedOK,websockets.exceptions.ConnectionClosedError,websockets.exceptions.ConnectionClosed):
-        #await self.event("disconnect")  # self.event_disconnect()
-        #sys.exit()
-        pass
-
-async def listen_event(ws):
-    events = []
-    for event in events:
-        await ws.send(json.dumps({
-            "body": {"eventName": event},
-            "header": {
-            "requestId": "00000000-0000-0000-0000-000000000000",
-            "messagePurpose": "subscribe",
-            "version": 1,
-            "messageType": "commandRequest"}
-            }))
-
-async def parse_command(message):
-        
-    if message["header"]["messagePurpose"] == "event":
-        event_name = message["header"]["eventName"]
-        await event(event_name, message)
-
-        if message["header"]["eventName"] == "PlayerMessage" and message["body"]["type"] == 'chat':
+    async def receive(self,websocket):
+        self.ws = websocket
+        await self.listen_event()
+        await self.event("connect")
+        print("test")
+        try:
+            while True:
+                data = await self.ws.recv()
+                await self.parse_command(json.loads(data))
+        except (websockets.exceptions.ConnectionClosedOK,websockets.exceptions.ConnectionClosedError,websockets.exceptions.ConnectionClosed):
+            #await self.event("disconnect")  # self.event_disconnect()
+            #sys.exit()
             pass
 
-    elif message["header"]["messagePurpose"] == "error":
-        await event("error", message)
+    async def listen_event(self):
+        events = ["PlayerMessage"]
+        for event in events:
+            await self.ws.send(json.dumps({
+                "body": {"eventName": event},
+                "header": {
+                "requestId": "00000000-0000-0000-0000-000000000000",
+                "messagePurpose": "subscribe",
+                "version": 1,
+                "messageType": "commandRequest"}
+                }))
 
-async def event(name, *args):
-    func = f"event_{name}"
-    if not func == "event_connect":
-        func = f"event_test"
+    async def command(self, cmd):
+        uuid4 = str(uuid.uuid4())
+        cmd_json = json.dumps({
+            "body": {"origin": {"type": "player"},"commandLine": cmd,"version": 1},
+            "header": {"requestId": uuid4,"messagePurpose": "commandRequest","version": 1,"messageType": "commandRequest"}
+        })
+    
+        await self.ws.send(cmd_json)
+        data = await self.ws.recv()
+        msg = json.loads(data)
+        if msg["header"]["messagePurpose"] == "commandResponse" and msg["header"]["requestId"] == uuid4:
+            return msg
+        else:
+            return None
 
-    if args == ():
-        try:
-            await eval(f"{func}()")
-        except NameError as e:
-            print(f"event_{name}")
+    async def parse_command(self,message):
+            
+        if message["header"]["messagePurpose"] == "event":
+            event_name = message["header"]["eventName"]
+            await self.event(event_name, message)
 
-    else:
-        try:
-            await eval(f"{func}({args})")
-        except NameError as e:
-            print(f"event_{name}")
+            if message["header"]["eventName"] == "PlayerMessage" and message["body"]["type"] == 'chat':
+                pass
 
-async def event_connect():
-    mcresptext = "§a[Server]§r welcome!"
-    #await self.command('tellraw @a {"rawtext":[{"text":"'+mcresptext+'"}]} ')
-    print("Connected!")
+        elif message["header"]["messagePurpose"] == "error":
+            await self.event("error", message)
 
-async def event_test(event):
-    print(event)
-    pass
+    async def event(self, name, *args):
+        func = f"self.event_{name}"
+        if not func == "self.event_connect":
+            func = f"self.event_test"
 
-async def main():
-    # Set the stop condition when receiving SIGTERM.
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+        if args == ():
+            try:
+                await eval(f"{func}()")
+            except NameError as e:
+                print(f"event_{name}")
 
-    async with websockets.serve(
-        receive,
-        host="0.0.0.0",
-        port=19132,
-        process_request=health_check,
-    ):
-        await stop
+        else:
+            try:
+                await eval(f"{func}({args})")
+            except NameError as e:
+                print(f"event_{name}")
+
+    async def event_connect(self):
+        mcresptext = "§a[Server]§r welcome!"
+        await self.command('tellraw @a {"rawtext":[{"text":"'+mcresptext+'"}]} ')
+        print("Connected!")
+
+    async def event_test(self,event):
+        print(event)
+        pass
+
+"""
+    async def main():
+        # Set the stop condition when receiving SIGTERM.
+        loop = asyncio.get_running_loop()
+        stop = loop.create_future()
+        loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+        async with websockets.serve(
+            receive,
+            host="0.0.0.0",
+            port=19132,
+            process_request=health_check,
+        ):
+            await stop
+"""
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    myobj = WsServer()
+    myobj.start()
